2f5f6d2abc35255f5a65204ac9bcbc7aaa0ff1b3
Anwesh43 <anweshthecool0@gmail.com>
2017-01-26 15:04:23 +0530

    Using the processtracker in app

--- app/src/main/java/com/anwesome/ui/processtrackerdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/processtrackerdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.anwesome.ui.processtrackerdemo;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anwesome.camera.processtracker.ProcessTracker;
+import com.anwesome.camera.processtracker.TrackProcess;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ProcessTracker processTracker = new ProcessTracker(this);
+        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.order),"Order Confirmed"));
+        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.onway),"On The Way"));
+        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.delivered),"Delivered"));
+        processTracker.show();
     }
 }
