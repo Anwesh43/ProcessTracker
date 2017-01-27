610c6ef1c54990ce514ba02e66ba4776476f73fa
Anwesh43 <anweshthecool0@gmail.com>
2017-01-27 12:36:12 +0530

    Adding TrackProcessCompletionListener in MainActivity

--- app/src/main/java/com/anwesome/ui/processtrackerdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/processtrackerdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.camera.processtracker.ProcessTracker;
 import com.anwesome.camera.processtracker.TrackProcess;
@@ -17,9 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         final ProcessTracker processTracker = new ProcessTracker(this);
-        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.order),"Order Confirmed"));
-        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.onway),"On The Way"));
-        processTracker.addProcess(new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.delivered),"Delivered"));
+        TrackProcess trackProcess = new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.order),"Order Confirmed");
+        trackProcess.setTrackProcessCompletionListener(new TrackProcess.TrackProcessCompletionListener() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"Order Confirmed",Toast.LENGTH_SHORT).show();
+            }
+        });
+        processTracker.addProcess(trackProcess);
+        TrackProcess trackProcess1 = new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.onway),"On The Way");
+        trackProcess1.setTrackProcessCompletionListener(new TrackProcess.TrackProcessCompletionListener() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"On the way",Toast.LENGTH_SHORT).show();
+            }
+        });
+        processTracker.addProcess(trackProcess1);
+        TrackProcess trackProcess2 = new TrackProcess(BitmapFactory.decodeResource(getResources(),R.drawable.delivered),"Delivered");
+        trackProcess2.setTrackProcessCompletionListener(new TrackProcess.TrackProcessCompletionListener() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"Delivered",Toast.LENGTH_SHORT).show();
+            }
+        });
+        processTracker.addProcess(trackProcess2);
         processTracker.show();
         new Thread(new Runnable() {
             @Override
